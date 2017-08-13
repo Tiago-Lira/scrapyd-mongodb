5e672b6e3e7649369412daf076c70fe4302a5f4d
Tiago Lira <tiagoliradsantos@gmail.com>
2017-08-13 13:27:03 -0300

    Changing version

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='scrapyd-mongodb',
-    version='0.1.0',
+    version='0.1.1',
     description='Scrapyd Queue Management with MongoDB',
     author='Tiago Lira',
     author_email='tiagolira.dev@gmail.com',
     license='MIT',
     url='https://github.com/Tiago-Lira/scrapyd-mongodb',
-    download_url='https://github.com/Tiago-Lira/scrapyd-mongodb/tarball/0.1.0',
+    download_url='https://github.com/Tiago-Lira/scrapyd-mongodb/tarball/0.1.1',
     keywords=['scrapy', 'scrapyd', 'mongodb', 'queue',
               'scrapyd-queue', 'scrapyd-backend', 'backend'],
     include_package_data=True,
